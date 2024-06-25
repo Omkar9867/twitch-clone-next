9ab39340f3455ba427d21cfb6fc5c1ad28c11a2b
Omkar <omkarrajan495@gmail.com>
2024-06-25 23:42:49 +0530

    Next.js app and implemented NavBar

---
+++ pages/_app.js
@@ -0,0 +1,12 @@
+import NavBar from "@/components/NavBar";
+import "../public/globals.css";
+
+// This default export is required in a new `pages/_app.js` file.
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <NavBar />
+      <Component {...pageProps} />
+    </>
+  );
+}
